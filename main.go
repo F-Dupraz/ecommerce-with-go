2cf2e39aff08ed836a15669861dffaf9620779e1
Fabricio Angel Dupraz <duprazfabricio@gmail.com>
2025-08-01 02:54:47 +0000

    skeleton created

--- main.go
+++ main.go
@@ -2,31 +2,17 @@ package main
 
 import (
   "fmt"
-  "net/http"
+  "context"
 
-  "github.com/go-chi/chi/v5"
-  "github.com/go-chi/chi/v5/middleware"
+  "github.com/F-Dupraz/ecommerce-with-go/application"
 )
 
 func main() {
-  router := chi.NewRouter()
+  app := application.New()
 
-  router.Use(middleware.Logger)
-
-  router.Get("/home", basicHandler)
-
-  server := &http.Server{
-	Addr: ":3000",
-	Handler: router,
-  }
-
-  err := server.ListenAndServe()
+  err := app.Start(context.TODO())
   if err != nil {
-	fmt.Println("Failed to listen to server: ", err)
+	fmt.Println("failed to start the server: %w", err)
   }
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("Hello World!"))
 }
 
